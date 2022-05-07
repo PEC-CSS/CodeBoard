db8ec8b191248961e7314203b9f357d65a66fe6c
Ansh Jain <69351886+Anshjain123@users.noreply.github.com>
2022-05-07 10:48:58 +0530

    Add files via upload

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,1 +1,13 @@
+import Check from "./Components/Check";
+import Editor from "./Components/Editor";
 
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+      {/* <Check/> */}
+    </div>
+  );
+}
+
+export default App;
