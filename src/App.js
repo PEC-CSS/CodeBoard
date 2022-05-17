2fdff3dbf8f59af3e6c264a44b411e8d92498ec4
Anshjain123 <anshjainalp@gmail.com>
2022-05-17 11:08:22 +0530

    Another Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Check from "./Components/Check";
 import Editor from "./Components/Editor";
 
 function App() {
   return (
     <div className="App">
       <Editor/>
-      {/* <Check/> */}
     </div>
   );
 }
--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,8 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import './Editor.css'
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/theme/monokai.css';
 import "codemirror/mode/clike/clike";
 import "codemirror/mode/python/python";
 import "codemirror/mode/javascript/javascript";
@@ -35,6 +33,7 @@ import "codemirror/addon/fold/comment-fold";
 import "codemirror/addon/fold/foldgutter.css";
 
 import "codemirror/lib/codemirror.css";
+import 'codemirror/theme/material.css';
 import "codemirror/theme/dracula.css";
 import "codemirror/theme/monokai.css";
 import "codemirror/theme/ambiance.css";
@@ -53,6 +52,8 @@ import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
 import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
 // import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 // import ReactMegaMenu from "react-mega-menu"
 require('codemirror/theme/neat.css');
 require('codemirror/mode/xml/xml.js');
@@ -77,10 +78,11 @@ function Editor() {
         }
         let temp = codeRef.current.editor.getValue()
         let code = temp.toString();
-    
+
     }
     useEffect(() => {
         const current = codeRef.current.editor.display.wrapper.style.height = '600px';
+
     }, [])
     const language = [
         "c++",
@@ -94,96 +96,201 @@ function Editor() {
         "java": "java",
         "python": "python"
     }
-
+    const Themes = ["ambiance", "monokai", "material", "dracula", "material-darker", "material-palenight", "mdn-like", "eclipse"]
+    const FontSizes = ["2px", "4px", "6px", "8px", "10px", "11px", "12px", "14px", "16px", "18px", "20px", "24px", "28px", "32px", "36px", "40px", "44px", "48px", "52px"];
+    const TabSizes = ["2", "4", "8"];
     const [mode, setmode] = useState('c++')
+    const [fontsize, setfontsize] = useState("16");
+    const [theme, settheme] = useState("ambiance")
+    const defaultsize = FontSizes[8];
     const defaultOption = language[0];
+    const defaulttheme = Themes[0];
+    const defaulttabsize = TabSizes[1];
+    const [tabsize, settabsize] = useState("4")
+    const handlechangefontsize = (e) => {
+        document.querySelector(".CodeMirror").style.fontSize = `${e.value}`;
+    }
+
     return (
-        <div className='flex'>
-            <Split >
-                <div className="whiteboard">
+        <>
+            <div className='flex' style={{ display: 'flex' }} >
 
-                </div>
-
-                <div className="editor">
-                    <div className="heading" style={{ height: '30px', width: '110px', marginBottom: '7px' }}>
-
-                        <div className="langaugeselector" >
-                            <Tooltip title="Choose Your Language" TransitionComponent={Zoom} placement='top'>
-
-                                <HelpOutlinedIcon style={{ marginTop: '7px', backgroundColor: 'white', fontWeight: 'lighter' }}>
-
-                                </HelpOutlinedIcon>
-
-                            </Tooltip>
-                            <Dropdown
-
-                                style={{ cursor: 'pointer' }}
-                                options={language}
-                                value={defaultOption}
-                                onChange={(e) => setmode(e.value)}
-                            />
-
-                        </div>
-
-
+                <Split >
+                    <div className="whiteboard">
+                        <h1>Area For WhiteBoard</h1>
                     </div>
-                    <Split horizontal initialPrimarySize="90%" minSecondarySize={size} splitterSize="0%" >
-                        <CodeMirror
-                            value="#include <bits/stdc++.h>"
-                            options={{
-                                mode: `text/x-${mapping[mode]}`,
-                                theme: 'monokai',
-                                lineNumbers: true,
-                                autoCloseBrackets: true,
-                                indentUnit: 4,
-                                autofocus: true,
-                                lineWrapping: true,
-                                foldGutter: true,
-                                autoCloseTags: true,
-                                matchBrackets: true,
-                                autoCloseBrackets: true,
-                                extraKeys: {
-                                    "Ctrl-Space": "autocomplete",
-                                },
-                                coverGutterNextToScrollbar: true,
-                                gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-                            }}
-                            ref={codeRef}
-                        />
-                        <div className="consoleplusarea">
-                            {/* <div className="testareaoutput">
+
+                    <div className="editor">
+                        <div className="heading" style={{ height: '30px', width: '100%', marginBottom: '7px' }}>
+
+                            <div className="langaugeselector">
+                                <Tooltip title="Choose Your Language" TransitionComponent={Zoom} placement='top'>
+
+                                    <HelpOutlinedIcon style={{ marginTop: '7px', backgroundColor: 'white', fontWeight: 'lighter' }}>
+
+                                    </HelpOutlinedIcon>
+
+                                </Tooltip>
+                                <Dropdown
+                                    style={{ cursor: 'pointer' }}
+                                    options={language}
+                                    value={defaultOption}
+                                    onChange={(e) => setmode(e.value)}
+                                />
+
+                            </div>
+                            <div className="settings" >
+                                <Tooltip title="Editor Settings" TransitionComponent={Zoom} placement='top'>
+
+                                    {/* <button type="button" className="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal" > */}
+                                    <SettingsIcon data-bs-toggle="modal" data-bs-target="#exampleModal" style={{ height: '20px', width: '20px' }} />
+                                    {/* </button> */}
+                                </Tooltip>
+                                {/* <!-- Modal --> */}
+                                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{ marginTop: '100px' }}>
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title" id="exampleModalLabel">Editors Settings</h5>
+                                                <button type="button" className="btn-close" style={{ outline: 'none', boxShadow: 'none' }} data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div className="modal-body">
+                                                <div className="fontsize">
+                                                    <div className="textdivide">
+                                                        <div className="head">
+                                                            Font
+                                                        </div>
+                                                        <div className="desc">
+                                                            Choose your preferred font size for the code editor.
+                                                        </div>
+                                                    </div>
+                                                    <div className="size" >
+                                                        <Dropdown
+                                                            style={{ cursor: 'pointer' }}
+                                                            options={FontSizes}
+                                                            value={defaultsize}
+                                                            onChange={handlechangefontsize}
+
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="modal-body">
+                                                <div className="fontsize">
+                                                    <div className="textdivide">
+                                                        <div className="head">
+                                                            Theme
+                                                        </div>
+                                                        <div className="desc">
+                                                            Choose your Favourite Theme.
+                                                        </div>
+                                                    </div>
+                                                    <div className="size">
+                                                        <Dropdown
+                                                            style={{ cursor: 'pointer' }}
+                                                            options={Themes}
+                                                            value={defaulttheme}
+                                                            onChange={(e) => settheme(e.value)}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="modal-body">
+                                                <div className="fontsize">
+                                                    <div className="textdivide">
+                                                        <div className="head">
+                                                            Tab Size
+                                                        </div>
+                                                        <div className="desc">
+                                                            Choose your preferred tab size.
+                                                        </div>
+                                                    </div>
+                                                    <div className="size">
+                                                        <Dropdown
+                                                            style={{ cursor: 'pointer' }}
+                                                            options={TabSizes}
+                                                            value={defaulttabsize}
+                                                            onChange={(e) => settabsize(e.value)}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <Split horizontal initialPrimarySize="90%" minSecondarySize={size} splitterSize="0%" >
+                            <CodeMirror
+                                value = "#include <iostream>"
+                                options={{
+                                    mode: `text/x-${mapping[mode]}`,
+                                    theme: `${theme}`,
+                                    lineNumbers: true,
+                                    autoCloseBrackets: true,
+                                    indentUnit: 8,
+                                    autofocus: true,
+                                    lineWrapping: true,
+                                    foldGutter: true,
+                                    autoCloseTags: true,
+                                    matchBrackets: true,
+                                    autoCloseBrackets: true,
+                                    tabSize: `${tabsize}`,
+                                    extraKeys: {
+                                        "Ctrl-Space": "autocomplete",
+                                    },
+                                    coverGutterNextToScrollbar: true,
+                                    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+                                }}
+                                ref={codeRef}
+                            />
+                            <div className="consoleplusarea">
+                                {/* <div className="testareaoutput">
                                 <div className="testarea">
 
                                 </div>
                                 <div className="output"></div>
                             </div> */}
-                            {!flag && <div className='testareaoutput'>
-                                <div className="testout">
-                                    <div className="testarea"></div>
-                                    <div className="output"></div>
+                                {!flag && <div className='testareaoutput'>
+                                    <div className="testout">
+                                        <div className="testarea"></div>
+                                        <div className="output"></div>
+                                    </div>
+                                    <textarea className="textarea">
+
+                                    </textarea>
+                                </div>}
+                                <div className="mainfooter" style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+                                    <Button className="Console" onClick={handleClicked} style={{ width: '80px', height: '20px' }}>
+                                        Console
+                                    </Button>
+                                    <div className="submitandtest">
+                                        <Button className="submit" >
+                                            <ArrowRightIcon/> Run Code
+                                        </Button>
+                                        <Button className="submit" >
+                                            Submit
+                                        </Button>
+                                    </div>
+
+                                    {/* <svg width="16px" marginTop="10px" height="13px" cursor='pointer' fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 19" preserveAspectRatio="xMidYMid meet"><path d="M7 8l-4 4 4 4M17 8l4 4-4 4M13 5l-2 14" stroke="#6c7f95" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg> */}
                                 </div>
-                                <textarea className="textarea">
-
-                                </textarea>
-                            </div>}
-                            <div className="mainfooter">
-
-                                <Button className="footer" onClick={handleClicked} style={{ width: '80px', height: '20px' }}>
-                                    Console
-                                </Button>
-                                {/* <svg width="16px" marginTop="10px" height="13px" cursor='pointer' fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 19" preserveAspectRatio="xMidYMid meet"><path d="M7 8l-4 4 4 4M17 8l4 4-4 4M13 5l-2 14" stroke="#6c7f95" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg> */}
                             </div>
 
+                        </Split>
+                    </div>
 
-                        </div>
-
-                    </Split>
-                </div>
-
-            </Split>
+                </Split>
+            </div>
 
 
-        </div>
+
+
+
+        </>
     )
 }
 
